e61dc41a4f5f5ab8acf07e67f36b7a1ce56c374d
Maciej Kopeć <kontakt@maciejkopec.pl>
2020-08-19 23:49:32 -0700

    enable getting images for unauthorized clients

--- src/main/java/pl/maciejkopec/cms/configuration/SecurityConfiguration.java
+++ src/main/java/pl/maciejkopec/cms/configuration/SecurityConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
@@ -45,6 +46,10 @@ public class SecurityConfiguration {
         .disable()
         .authorizeExchange()
         .pathMatchers("/mailer")
+        .permitAll()
+        .and()
+        .authorizeExchange()
+        .pathMatchers(HttpMethod.GET, "/images/*")
         .permitAll()
         .and()
         .authorizeExchange()
